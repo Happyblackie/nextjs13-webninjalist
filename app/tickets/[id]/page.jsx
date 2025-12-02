717a8929d0088555a27f1c70267fcce211d4e6ef
Happyblackie <happyblackie01@gmail.com>
2025-12-02 12:00:01 +0300

    Initial commit of project files updated again

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,67 +1,47 @@
+
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { notFound } from 'next/navigation';
-import React from 'react'
-
-//not found constant
-export const dynamicParams = true
-
-export async function generateStaticParams(){
-
-  const res = await fetch('http://localhost:4000/tickets');
-
-  const tickets = await res.json()
-
-  return tickets.map((ticket) => ({
-      id: ticket.id
-  }))
-}
-
-
+import React from 'react';
 
 //fetch ticket data from local json file
-async function getTicket(id)
-{
-    //imitate delay(loading ui & suspense)
-   await new Promise(resolve => setTimeout(resolve, 3000))
+async function getTicket(id) {
+  //imitate delay (loading ui & suspense)
+  await new Promise(resolve => setTimeout(resolve, 3000));
 
-                            //you can use a template string as well:  (`http://localhost:4000/tickets/${id}`)
-    const res = await fetch('http://localhost:4000/tickets/'+ id , { 
-      next: {
-        revalidate: 0
-      } 
-    });
+  const res = await fetch('http://localhost:4000/tickets/' + id, {
+    cache: 'no-store'
+  });
 
-     //not found page for ticket that doesnt exist
-    if(!res.ok){
-      notFound()
-    }
-    
-    return res.json();
+  //not found page for ticket that doesnt exist
+  if (!res.ok) {
+    notFound();
+  }
 
-
+  return res.json();
 }
 
-
-
-export default async function TicketDetails({params}) {
-
-    //const id = params.id;
-    const ticket = await getTicket(params.id)
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id);
 
   return (
-    //<div>TicketDetails {id} </div>
     <main>
-        <nav>
-            <h2>Ticket Details</h2>
-        </nav>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
 
-        <div className="card">
-            <h3>{ticket.title}</h3>
-            <small>Created by {ticket.user_email}</small>
-            <p>{ticket.body}</p>
-            <div className={`pill ${ticket.priority}`}>  
-                    {ticket.priority} priority
-            </div>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
         </div>
+      </div>
     </main>
-  )
+  );
 }
+
+
